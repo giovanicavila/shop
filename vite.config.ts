204b02b5746274d19cf6a95eed0ac1c7d8a0d8da
Giovani <giovani.avila.dev@gmail.com>
2025-12-02 08:56:01 -0300

    feat: add rollup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -14,10 +15,26 @@ export default defineConfig({
 		}),
 		react(),
 		tailwindcss(),
+		visualizer({
+			open: true,
+			gzipSize: true,
+			brotliSize: true,
+			filename: "dist/stats.html",
+		}),
 	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					"react-vendor": ["react", "react-dom"],
+					router: ["@tanstack/react-router"],
+				},
+			},
 		},
 	},
 });
